37245f5f4ed33795fcf8ce80d8d7b693dbbdf81c
Ken Bayus <kennethbayus@gmail.com>
2016-05-01 19:21:47 -0400

    posts and users working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+	 	if @user.update(user_params)
+  		flash['success'] = "User was updated successfully"
+  		redirect_to root_path
+  	else
+  		render :edit
+  	end
+
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:username, :printname, :password)
+		end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  belongs_to :user
 
   validates :title, presence: true, uniqueness: true
 	validates :body, presence: true
